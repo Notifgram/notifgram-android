b157256cb7ee6597c60058621512236e78bceb17
Notifgram <notifgram6@gmail.com>
2025-10-13 11:26:08 -0800

    add compose bom to ":core:test"

--- core/test/build.gradle.kts
+++ core/test/build.gradle.kts
@@ -28,6 +28,7 @@ dependencies {
     api(libs.kotlinx.coroutines.test)
     api(libs.turbine)
     debugApi(libs.androidx.compose.ui.testManifest)
+    implementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.testManifest)
 //    debugApi(libs.androidx.compose.ui.testManifest)
 //    implementation(libs.core.ktx)
